d6cba483494ee9cadab70abf1c28c32b0d13cd96
MocaCDev <mocacdeveloper@gmail.com>
2024-09-21 15:03:34 -0400

    Initial Commit

---
+++ EZNotes-2/EZNotes_2App.swift
@@ -0,0 +1,17 @@
+//
+//  EZNotes_2App.swift
+//  EZNotes-2
+//
+//  Created by Aidan White on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EZNotes_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EZNotes-2Tests/EZNotes_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  EZNotes_2Tests.swift
+//  EZNotes-2Tests
+//
+//  Created by Aidan White on 9/21/24.
+//
+
+import Testing
+@testable import EZNotes_2
+
+struct EZNotes_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
