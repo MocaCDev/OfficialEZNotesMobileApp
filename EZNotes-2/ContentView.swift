af22e717d4893dd567b04aaf8e5028b1b6cd6abe
MocaCDev <mocacdeveloper@gmail.com>
2024-10-17 23:31:53 -0400

    added faceID suppot

--- EZNotes-2/ContentView.swift
+++ EZNotes-2/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import Combine
 import UIKit
+import LocalAuthentication
+
 //import PhotosUI
 //import UIKit
 
@@ -35,7 +37,37 @@ extension KeyboardReadable {
 }
 
 struct ContentView: View {
+    @State private var faceIDAuthenticated: Bool = false
+    @Environment(\.scenePhase) private var scenePhase
+    
+    private func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "FaceID is needed to allow access to the app."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    self.faceIDAuthenticated = true
+                } else {
+                    self.goBackToLogin = true
+                    self.userHasSignedIn = false
+                    
+                    UserDefaults.standard.removeObject(forKey: "logged_in")
+                    UserDefaults.standard.removeObject(forKey: "requires_faceID")
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+    
     @State public var userHasSignedIn: Bool = UserDefaults.standard.bool(forKey: "logged_in")
+    @State private var goBackToLogin: Bool = false
     @StateObject private var model: FrameHandler = FrameHandler()
     
     @State private var categoriesAndSets: [String: Array<String>] = getCategoryData() /* MARK: Key will be the category name, value will be the set names */
@@ -57,17 +89,37 @@ struct ContentView: View {
             )
         } else {
             VStack {
-                ResponsiveView { prop in
-                    CoreApp(
-                        model: model,
-                        prop: prop,
-                        categoriesAndSets: $categoriesAndSets,
-                        categoryCreationDates: $categoryCreationDates,
-                        categoryImages: $categoryImages,
-                        categoryDescriptions: $categoryDescriptions,
-                        categoryCustomColors: $categoryCustomColors,
-                        categoryCustomTextColors: $categoryCustomTextColors
-                    )
+                if self.faceIDAuthenticated {
+                    ResponsiveView { prop in
+                        CoreApp(
+                            model: model,
+                            prop: prop,
+                            categoriesAndSets: $categoriesAndSets,
+                            categoryCreationDates: $categoryCreationDates,
+                            categoryImages: $categoryImages,
+                            categoryDescriptions: $categoryDescriptions,
+                            categoryCustomColors: $categoryCustomColors,
+                            categoryCustomTextColors: $categoryCustomTextColors
+                        )
+                    }
+                } else {
+                    if self.goBackToLogin {
+                        StartupScreen(
+                            userHasSignedIn: $userHasSignedIn
+                        )
+                    } else {
+                        VStack {
+                            Text("Unlock With FaceID")
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                .foregroundStyle(.white)
+                                .font(.system(size: 30, design: .rounded))
+                                .fontWeight(.bold)
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(
+                            Image("Background")
+                        )
+                    }
                 }
                 /* TODO: Add "Core.swift". */
                 //ResponsiveView { prop in
@@ -84,6 +136,19 @@ struct ContentView: View {
                     //}//UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
                 //}
             }
+            .onChange(of: scenePhase) { oldPhase, newPhase in
+                if newPhase == .inactive || newPhase == .background {
+                    UserDefaults.standard.set(true, forKey: "requires_faceID")
+                }
+            }
+            .onAppear(perform: {
+                if UserDefaults.standard.bool(forKey: "requires_faceID") {
+                    self.faceIDAuthenticated = false
+                    authenticate()
+                } else {
+                    self.faceIDAuthenticated = true
+                }
+            })
         }
     }
 }
